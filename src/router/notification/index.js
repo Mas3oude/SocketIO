036355140854cf876f24e79416bddd4d909248ab
Mohamed <eng.massoud@hotmail.com>
2021-03-23 10:43:23 +0100

    dockerfile with some extra logs

--- src/router/notification/index.js
+++ src/router/notification/index.js
@@ -1,9 +1,25 @@
 const { Router } = require('express');
-
+const path = require('path');
 const router = new Router();
 router.get('/',(req,res)=>{
     res.send(`this is the default router thank you ..`);
 });
+
+router.get('/static',(req,res)=>{
+    // res.sendFile('../../../public/user1.html');
+    const routerPath = __dirname.split('/');
+    routerPath.pop(routerPath.length-1);
+    routerPath.pop(routerPath.length-1);
+    routerPath.pop(routerPath.length-1);
+    let rootpath='/';
+    for (let i = 1; i < routerPath.length; i++) {
+       rootpath += routerPath[i] + '/';
+        
+    }
+    console.log(`the path for static file : ${rootpath+'public/testing.html'}`);
+    console.log(path.join(rootpath+'public/user1.html'));
+    res.sendFile(path.join(rootpath+'public/user1.html'));
+  });
 
 
 module.exports = router;
